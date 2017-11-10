53dc9ce6b1e121878d4be4e340815a71c2beaa55
Gumball Studios <gumballstudios@gmail.com>
2017-11-10 14:38:05 -0500

    Prototype of the collision system
    Initial prototype of handling collisions between projectiles moving into
    the player's area and the player moving into a blocking projectile

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,14 @@
 extends Node
 
 
+const widget_offset = Vector2(32, 0)
+
 var min_frame = 0
 var max_frame = 4
 var current_frame = 0
 
 onready var position_list = get_node("Positions")
+onready var collision_widget = get_node("CollisionWidget")
 
 
 func _ready():
@@ -30,4 +33,11 @@ func move(direction):
 	position_list.get_child(current_frame).hide()
 	position_list.get_child(new_frame).show()
 	current_frame = new_frame
+	
+	var new_pos = collision_widget.get_pos() + (widget_offset * direction)
+	collision_widget.set_pos(new_pos)
+	var detector = collision_widget.get_node("Detect")
+	detector.force_raycast_update()
+	if detector.is_colliding():
+		print("player hit projectile")
 
--- Scripts/ProjectileGroup.gd
+++ Scripts/ProjectileGroup.gd
@@ -2,19 +2,34 @@ extends Node
 
 
 var projectile_index = 0
+var widget_offset = Vector2(0, 16)
+
+onready var sprite_list = get_node("Sprites")
+onready var collision_widget = get_node("CollisionWidget")
 
 
 func _ready():
-	var projectile = get_child(projectile_index)
+	var projectile = sprite_list.get_child(projectile_index)
 	projectile.show()
 
 
 func _on_tick():
-	var previous_projectile = get_child(projectile_index)
+	var previous_projectile = sprite_list.get_child(projectile_index)
 	projectile_index += 1
-	if projectile_index < get_child_count():
-		var next_projectile = get_child(projectile_index)
-		previous_projectile.hide()
-		next_projectile.show()
-	else:
+	
+	if projectile_index >= sprite_list.get_child_count():
 		queue_free()
+		return
+	
+	var next_projectile = sprite_list.get_child(projectile_index)
+	previous_projectile.hide()
+	next_projectile.show()
+	
+	var new_pos = collision_widget.get_pos() + widget_offset
+	collision_widget.set_pos(new_pos)
+	
+	var detector = collision_widget.get_node("Detect")
+	detector.force_raycast_update()
+	if detector.is_colliding():
+		print("projectile hit player")
+
